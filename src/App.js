9d51695f472c3bbe446fb9cfa663d68cd366cce6
Divy <divyparekh1810@gmail.com>
2022-06-16 18:07:48 +0530

    Done with router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/home/Home';
 import List from './pages/list/List';
 import Login from './pages/login/Login';
+import New from './pages/new/New';
 import Single from './pages/single/Single';
 const App = () => {
   return (
-    <div>
-      <Home />
-      <List />
-      <Login />
-      <Single />
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            <Route index element={<Home />} exact/>
+            <Route path='/Login' element={<Login />} exact/>
+            <Route path='users'>
+              <Route index element={<List/>} exact/>
+              <Route path=':userId' element={<Single/>} exact/>
+              <Route path='new' element={<New/>} exact/>
+            </Route>
+            <Route path="products">
+              <Route index element={<List/>} exact/>
+              <Route path=':productId' element={<Single/>} exact/>
+              <Route path='new' element={<New/>} exact/>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   )
 }
